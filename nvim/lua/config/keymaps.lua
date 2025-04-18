6434c1dcb44d0a8ee0a4a07f1e1c552d44e106bd
Pedro Altuve <paltuveg@gmail.com>
2025-04-18 12:45:27 +0100

    Added keybinds to close tabs and git blame

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,5 @@
 vim.keymap.set("n", "<leader>y", '"+yy', { desc = "Yank line to clipboard" })
 vim.keymap.set("v", "<leader>y", '"+y', { desc = "Yank selection to clipboard" })
 vim.keymap.set("n", "<leader>Y", 'gg"+yG', { desc = "Yank entire buffer to clipboard" })
+vim.keymap.set("n", "<leader>x", ":tabclose<CR>", { desc = "Close tab" })
+vim.keymap.set("n", "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Git Blame" })
