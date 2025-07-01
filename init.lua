dc5254aff3965683a247bd305e82a63d44313cca
Pedro Altuve <paltuveg@gmail.com>
2025-07-01 12:06:50 +0100

    Added init file

---
+++ init.lua
@@ -0,0 +1,28 @@
+vim.opt.termguicolors = true
+require("config.lazy")
+require("config.keymaps")
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
